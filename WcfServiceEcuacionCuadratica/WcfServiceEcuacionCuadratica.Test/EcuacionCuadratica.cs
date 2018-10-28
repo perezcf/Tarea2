48ac735e000251811aacda0050a643b795cdbd85
gmg <gmg@LAPTOP-BH039M2G>
2018-10-28 17:22:56 -0600

    commit del testting

---
+++ WcfServiceEcuacionCuadratica/WcfServiceEcuacionCuadratica.Test/EcuacionCuadratica.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WcfServiceEcuacionCuadratica.Test
+{
+    [TestClass]
+    public class EcuacionCuadratica
+    {
+        [TestMethod]
+        public void ParametroAPositivoDiscriminantePositivo()
+        {
+
+            //// preparación del escenario
+            var a = 1.0;
+            var b = -2.0;
+            var c = -24.0;
+            var miValorEsperado = 6 + -4;
+            var miValorReal = 0.0;
+
+            // invocamos el método
+            // para invocar dinámicamente al método
+            var elServicio = new WcfServiceEcuacionCuadratica.Service1();
+            miValorReal = elServicio.EcuacionCuadratica(a, b, c);
+
+            //verificar el resultado obtenido
+            Assert.AreEqual(miValorEsperado, miValorReal);
+
+
+
+        }
+    }
+}
---
+++ WcfServiceEcuacionCuadratica/WcfServiceEcuacionCuadratica.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WcfServiceEcuacionCuadratica.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("WcfServiceEcuacionCuadratica.Test")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("33e20b33-1200-4f5b-81be-d08776f50171")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
